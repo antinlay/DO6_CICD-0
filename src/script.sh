20f67870ccb18de011b4f8ae8b02e4f16808a102
Janiece Eleonour <janiecee@ws11.com>
2023-02-15 20:26:13 +0300

    develop STYLE TEST CHECK

--- src/script.sh
+++ src/script.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-if ! [ -d "./C3_SimpleBashUtils-0" ]
-then 
-    git clone ssh://git@repos-ssh.21-school.ru:2289/students/C3_SimpleBashUtils.ID_353513/janiecee_student.21_school.ru/C3_SimpleBashUtils-0.git -b develop
-fi
+output=$(clang-format -style="{$(grep ../ BasedOnStyle)}" -n $(find ../ -name *.[ch]))
+
+if [ -n "$output" ]; then echo "$output" && exit 1; fi
+
+# if ! [ -d "./C3_SimpleBashUtils-0" ]
+# then 
+#     git clone ssh://git@repos-ssh.21-school.ru:2289/students/C3_SimpleBashUtils.ID_353513/janiecee_student.21_school.ru/C3_SimpleBashUtils-0.git -b develop
+# fi

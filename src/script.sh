9761488f70a4f379ac490551edaadf52f449b663
janiecee <janiecee@do4.com>
2023-02-15 06:20:18 +0000

    develop PART 2 OK

---
+++ src/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if ! [ -d "./C3_SimpleBashUtils-0" ]; then 
+    git clone ssh://git@repos-ssh.21-school.ru:2289/students/C3_SimpleBashUtils.ID_353513/janiecee_student.21_school.ru/C3_SimpleBashUtils-0.git -b develop
+fi

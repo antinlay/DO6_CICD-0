a2bceccc97222e02cf74b7a80f88da1ac66cdff2
Janiece Eleonour <janiecee@ws11.com>
2023-02-16 20:49:10 +0300

    develop TEST DEPLOY PUBLISHKEY SOLVE WITHOUT SCP FLAGS

--- src/script.sh
+++ src/script.sh
@@ -2,24 +2,28 @@
 
 set -e
 
-REMOTE_HOST="192.168.1.2"
-REMOTE_USER="root"
-REMOTE_PSW="2121"
-REMOTE_PATH="/usr/local/bin"
+scp src/C3_SimpleBashUtils-0/cat/s21_cat janiecee@192.68.1.2:/usr/local/bin
+scp src/C3_SimpleBashUtils-0/grep/s21_grep janiecee@192.68.1.2:/usr/local/bin
+# REMOTE_HOST="192.168.1.2"
+# REMOTE_USER="root"
+# REMOTE_PSW="2121"
+# REMOTE_PATH="/usr/local/bin"
 
 # ARTHEFACT_FILES=$(find $CI_PROJECT_DIR -type f -executable -name s21_*)
-ARTHEFACT_FILE_1="src/C3_SimpleBashUtils-0/cat/s21_cat"
-ARTHEFACT_FILE_2="src/C3_SimpleBashUtils-0/grep/s21_grep"
+# ARTHEFACT_FILE_1="src/C3_SimpleBashUtils-0/cat/s21_cat"
+# ARTHEFACT_FILE_2="src/C3_SimpleBashUtils-0/grep/s21_grep"
 
-echo $ARTHEFACT_FILE_1 $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
-echo $ARTHEFACT_FILE_2 $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+
+
+# echo $ARTHEFACT_FILE_2 $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+
 # ssh-copy-id $REMOTE_USER@$REMOTE_HOST
 # ssh $REMOTE_USER@$REMOTE_HOST "echo $REMOTE_PSW | sudo chown -R $REMOTE_USER@$REMOTE_HOST /usr/local/bin"
-scp $ARTHEFACT_FILE_1 $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+# scp $ARTHEFACT_FILE_1 $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
 
-scp $ARTHEFACT_FILE_2 $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+# scp $ARTHEFACT_FILE_2 $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
 
-exit 1
+# exit 1
 # output=$(clang-format -style="{$(grep ../ BasedOnStyle)}" -n $(find ../ -name *.[ch]))
 
 # output=$(find . -name '*_test.sh' -type f -print0 | while IFS= read -r -d $'\0' file; do bash "$file"; done 2>&1)

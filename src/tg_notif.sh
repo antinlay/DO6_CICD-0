a26c598622935bae3b0f29effcbe2b8b99e14e8a
Janiece Eleonour <janiecee@ws11.com>
2023-02-17 14:32:50 +0300

    develop TG NOTE FIX PATH ENV

--- src/tg_notif.sh
+++ src/tg_notif.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-. .env
 
-echo $TELEGRAM_USER_ID $TELEGRAM_BOT_TOKEN
+ENV=$(find . -type f -name .env)
+. $ENV
+
+# echo $TELEGRAM_USER_ID $TELEGRAM_BOT_TOKEN
 TIME=$(date)
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
 TEXT="$TIME Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"

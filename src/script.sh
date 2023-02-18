e82ccd19eb5a9a4771d4f984b5aa131b558e8437
Janiece Eleonour <janiecee@ws11.com>
2023-02-18 18:29:54 +0300

    develop CONDITION WITH KNOW HOST TRY 2

--- src/script.sh
+++ src/script.sh
@@ -4,7 +4,7 @@
 REMOTE_MAC="08:00:27:79:35:c2"
 INTERFACE=$(ip route list | awk '/^default/ {print $5}');
 IP=$(ip -o -f inet addr show $INTERFACE | awk '/scope global/ {print $4}' | cut -d/ -f1);
-fping -a -g "$IP/24" > /dev/null;
+fping -a -g "$IP/24" 2>/dev/null;
 REMOTE_HOST=$(arp -e | grep $REMOTE_MAC | awk '{print $1}');
 REMOTE_USER="janiecee"
 # REMOTE_PSW="2121"
@@ -15,9 +15,11 @@ ARTHEFACT_FILE=$(find . -type f -executable -name s21_cat -name s21_grep)
 
 echo $ARTHEFACT_FILE $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
 # echo $ARTHEFACT_FILE_2 $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
-if ! ssh-keygen -F $REMOTE_HOST > /dev/null; then 
-    ssh-keyscan -t ecdsa $REMOTE_HOST >> ~/.ssh/known_hosts
+if ssh-keygen -F $REMOTE_HOST > /dev/null; then 
+    echo "$REMOTE_HOST already in ~/.ssh/known_hosts";
+else
+    ssh-keyscan -t ecdsa $REMOTE_HOST >> ~/.ssh/known_hosts;
 fi
 
-scp $ARTHEFACT_FILE $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+scp $ARTHEFACT_FILE $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH;
 # scp $ARTHEFACT_FILE_2 $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH

cd733e3e10fc25ea9d4a91e948f265d163065dfe
Janiece Eleonour <janiecee@ws11.com>
2023-02-18 11:10:16 +0300

    develop FIX SCRIPT SSH FROM MAC

--- src/script.sh
+++ src/script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-REMOTE_HOST="192.168.179.159"
+REMOTE_MAC="08:00:27:79:35:c2"
+INTERFACE=$(ip route list | awk '/^default/ {print $5}');
+IP=$(ip -o -f inet addr show $INTERFACE | awk '/scope global/ {print $4}' | cut -d/ -f1);
+
+REMOTE_HOST=$(arp-scan --interface=$INTERFACE --localnet | grep $REMOTE_MAC | awk '{print $1}');
 REMOTE_USER="janiecee"
 # REMOTE_PSW="2121"
 REMOTE_PATH="/usr/local/bin/"
